dcbdb5f20e5b13087c163983f12770892f966b49
Juan David Sánchez <juancho85@gmail.com>
2017-02-19 20:29:11 +0100

    Redirect to index if reload on routes different that the root

---
+++ src/main/java/com/authexample/WebConfiguration.java
@@ -0,0 +1,19 @@
+package com.authexample;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.web.servlet.ErrorPage;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by juancho on 19/02/2017.
+ */
+@Component
+public class WebConfiguration implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer configurableEmbeddedServletContainer) {
+        configurableEmbeddedServletContainer.addErrorPages(new ErrorPage(HttpStatus.NOT_FOUND, "/index.html"));
+    }
+}
